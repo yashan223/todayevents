8d59d624a9a72e7f69944c62799807ee4eecd3b3
yashan223 <yashanpererax200302@gmail.com>
2026-01-10 09:49:15 +0530

    allow host and titile change

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+    allowedHosts: ["todayevents.lk"],
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
